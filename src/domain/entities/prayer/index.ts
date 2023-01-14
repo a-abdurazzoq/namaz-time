8f2889cdf8edb83440a4e897d8d58cd2ccecd317
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-14 18:58:09 +0500

    [+] Create Prayer entity

---
+++ src/domain/entities/prayer/index.ts
@@ -0,0 +1,51 @@
+export class Prayer {
+    constructor(
+        private name: string,
+        private time: Date,
+        private createAt: Date,
+        private updateAt: Date
+    ) {
+        this.isName(name)
+        this.isTime(time)
+        this.isCreateAt(createAt)
+        this.isUpdateAt(updateAt)
+    }
+
+    public getName(): string {
+        return this.name
+    }
+    public getTime(): Date {
+        return this.time
+    }
+    public getCreateAt(): Date {
+        return this.createAt
+    }
+    public getUpdateAt(): Date {
+        return this.updateAt
+    }
+
+    private isName(name: string): void {
+        if(name.constructor !== String) {
+            throw new Error("name is not string")
+        }
+    }
+
+    private isTime(time: Date): void {
+        if(time.constructor !== Date) {
+            throw new Error("time is not date")
+        }
+    }
+
+    private isCreateAt(createAt: Date): void {
+        if(createAt.constructor !== Date) {
+            throw new Error("createAt is not date")
+        }
+    }
+
+    private isUpdateAt(updateAt: Date): void {
+        if(updateAt.constructor !== Date) {
+            throw new Error("updateAt is not date")
+        }
+    }
+
+}
