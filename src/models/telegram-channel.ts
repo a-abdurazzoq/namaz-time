5e3c6eddb14637d0322c21c5e349c050b76b62e4
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-17 01:46:57 +0500

    [+] Realization telegram channel model

---
+++ src/models/telegram-channel.ts
@@ -0,0 +1,33 @@
+import {Schema, model, Types} from "mongoose"
+
+export interface ITelegramChannelModel {
+    name: string;
+    mosque_id: Types.ObjectId;
+    chat_id: number;
+    create_at: Date;
+    update_at: Date;
+}
+
+const TelegramChannelSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    mosque_id: {
+        type: Types.ObjectId,
+        required: true,
+    },
+    chat_id: {
+        type: Number,
+    },
+    create_at: {
+        type: Date,
+        default: Date.now()
+    },
+    update_at: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+export const TelegramChannelModel = model<ITelegramChannelModel>("telegram-channels", TelegramChannelSchema)
