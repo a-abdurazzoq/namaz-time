c9b067531d5fcf2b4c1c72436cd98574ac9af886
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-17 01:47:06 +0500

    [+] Realization template photo model

---
+++ src/models/template-photo.ts
@@ -0,0 +1,30 @@
+import {Schema, model, Types} from "mongoose"
+
+export interface ITemplatePhotoModel {
+
+    file_name: string;
+    user_id: Types.ObjectId;
+    create_at: Date;
+    update_at: Date;
+}
+
+const TemplatePhotoSchema = new Schema({
+    file_name: {
+        type: String,
+        required: true
+    },
+    user_id: {
+        type: Types.ObjectId,
+        required: true
+    },
+    create_at: {
+        type: Date,
+        default: Date.now()
+    },
+    update_at: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+export const TemplatePhotoModel = model<ITemplatePhotoModel>("template-photos", TemplatePhotoSchema)
