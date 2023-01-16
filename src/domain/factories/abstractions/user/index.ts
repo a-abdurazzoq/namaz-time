ce65a6f463f7a67e8e459dc8e01323f54e611f83
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-16 23:02:05 +0500

    [+] Realization abstract UserFactory

---
+++ src/domain/factories/abstractions/user/index.ts
@@ -0,0 +1,13 @@
+import {Factory} from "../factory";
+import {User} from "../../../entities/user";
+
+export interface UserDto {
+    username: string;
+    password: string;
+    fullName: string;
+    phoneNumber: string;
+    createAt: Date;
+    updateAt: Date;
+}
+
+export interface UserFactory extends Factory<UserDto, User> {}
