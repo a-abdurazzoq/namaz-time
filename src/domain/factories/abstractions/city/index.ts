51468bd4aeaada68ed024604a20047d0d5cdd595
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-19 03:21:43 +0500

    [+] Realization abstract CityFactory

--- src/domain/factories/abstractions/city/index.ts
+++
@@ -1,9 +0,0 @@
-import {Factory} from "../factory";
-import {City} from "../../../entities/city";
-
-export interface CityDto {
-    id: string;
-    name: string;
-}
-
-export interface CityFactory extends Factory<CityDto, City> {}
