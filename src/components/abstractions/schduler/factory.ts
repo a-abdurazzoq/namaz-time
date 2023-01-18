bb12efa28448139aebbfc5b8d7ef13994bebe9a8
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-19 03:41:25 +0500

    [+] Realization abstract factory for scheduler

---
+++ src/components/abstractions/schduler/factory.ts
@@ -0,0 +1,12 @@
+import {Factory} from "../../../domain/abstractions/factories/factory";
+import {Scheduler} from "../../schduler";
+import {Config} from "./config";
+import {Controller} from "../../../controllers/abstractions";
+
+export interface SchedulerDto {
+    config: Config;
+    controller: Controller;
+    logger: any;
+}
+
+export interface SchedulerFactory extends Factory<SchedulerDto, Scheduler> {}
