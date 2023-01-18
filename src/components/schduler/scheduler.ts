91099bd63c80c3438011f47f22e2ebd4e953774a
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-19 03:43:40 +0500

    [+] Realization scheduler

---
+++ src/components/schduler/scheduler.ts
@@ -0,0 +1,55 @@
+import {Config} from "../abstractions/schduler";
+import {Controller} from "../../controllers/abstractions";
+
+export class Scheduler {
+    private timer: NodeJS.Timer
+
+    constructor(
+        private config: Config,
+        private controller: Controller,
+        private logger: Logger
+    ) {}
+
+    public async start(): Promise<void> {
+        await this.executeTime()
+        return
+    }
+
+    public async stop(): Promise<void> {
+        this.unsetTimer()
+        return
+    }
+
+    private async executeTime(): Promise<void> {
+        try {
+            let result = await this.controller.execute()
+
+            await this.logger.print({result: result})
+        }
+        catch (error) {
+            await this.logger.print({error: error})
+        }
+        finally {
+            this.setTimer()
+        }
+        return
+    }
+
+    private setTimer(): void {
+        this.unsetTimer()
+
+        this.timer = setTimeout(this.executeTime.bind(this), this.getNextTime())
+        return
+    }
+
+    private unsetTimer(): void {
+        if(!this.timer)
+            clearTimeout(this.timer)
+        return
+    }
+
+    private getNextTime(): number {
+        return this.config.getTimeInterval()
+    }
+
+}
