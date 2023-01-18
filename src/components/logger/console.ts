1a6323c1bcdf54c686974a8824427b00cd682204
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-19 03:32:38 +0500

    [+] Implementation console logger

---
+++ src/components/logger/console.ts
@@ -0,0 +1,19 @@
+export class ConsoleLogger implements Logger {
+    public async print(logData: LogData): Promise<void> {
+        if (!logData.date) logData.date = new Date()
+
+        if(logData.error)
+            this.printError(logData)
+
+        if(logData.result)
+            this.printResult(logData)
+    }
+
+    private printError(logData: Omit<LogData, "result">) {
+        console.log(logData.date, "-", "ERROR", "-", logData.error)
+    }
+
+    private printResult(logData: Omit<LogData, "error">) {
+        console.log(logData.date, "-", "RESULT", "-", logData.result)
+    }
+}
