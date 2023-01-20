8815f1824cd3b05db593fd5755ab9e44c2a7064e
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-21 02:35:00 +0500

    [+] Implementation UserRepository

---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,35 @@
+import {UserRepository} from "./abstractions";
+import {inject, injectable} from "inversify";
+import {UserFactory} from "../domain/abstractions/factories";
+import {Symbols} from "../dependencies/symbols";
+import {User} from "../domain/entities";
+import {UserModel, IUserModel} from "../models";
+
+@injectable()
+export class UserRepositoryImpl implements UserRepository {
+    constructor(
+        @inject<UserFactory>(Symbols.Factories.User) private userFactory: UserFactory
+    ) {}
+
+    public async getById(id: string): Promise<User> {
+        let getUser = await UserModel.findById<IUserModel>(id)
+
+        if(!getUser)
+            throw new Error("User not found by id")
+
+        return this.toEntity(getUser)
+    }
+
+    private toEntity(userModel: IUserModel): User {
+        return this.userFactory.create({
+            id: userModel._id.toHexString(),
+            fullName: userModel.full_name,
+            phoneNumber: userModel.phone_number,
+            username: userModel.username,
+            password: userModel.password,
+            createAt: userModel.create_at,
+            updateAt: userModel.update_at
+        })
+    }
+
+}
