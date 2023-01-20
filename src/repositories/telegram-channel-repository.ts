5bb5aa78f0cabc49590ed66f7154315e493b5370
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-21 02:34:51 +0500

    [+] Implementation TelegramChannelRepository

---
+++ src/repositories/telegram-channel-repository.ts
@@ -0,0 +1,37 @@
+import {MosqueRepository, TelegramChannelRepository} from "./abstractions";
+import {inject, injectable} from "inversify";
+import {Symbols} from "../dependencies/symbols";
+import {TelegramChannel} from "../domain/entities";
+import {TelegramChannelModel, ITelegramChannelModel} from "../models";
+import {TelegramChannelFactory} from "../domain/abstractions/factories";
+
+@injectable()
+export class TelegramChannelRepositoryImpl implements TelegramChannelRepository {
+    constructor(
+        @inject<TelegramChannelFactory>(Symbols.Factories.TelegramChannel) private telegramChannelFactory: TelegramChannelFactory,
+        @inject<MosqueRepository>(Symbols.Repositories.Mosque) private mosqueRepository: MosqueRepository
+    ) {}
+
+    public async getById(id: string): Promise<TelegramChannel> {
+        let getTelegramChannel = await TelegramChannelModel.findById<ITelegramChannelModel>(id)
+
+        if(!getTelegramChannel)
+            throw new Error("Telegram channel not found by id")
+
+        return this.toEntity(getTelegramChannel)
+    }
+
+    private async toEntity(telegramChannelModel: ITelegramChannelModel): Promise<TelegramChannel> {
+        let mosque = await this.mosqueRepository.getById(telegramChannelModel.mosque_id.toHexString())
+
+        return this.telegramChannelFactory.create({
+            id: telegramChannelModel._id.toHexString(),
+            name: telegramChannelModel.name,
+            chatId: telegramChannelModel.chat_id,
+            mosque: mosque,
+            createAt: telegramChannelModel.create_at,
+            updateAt: telegramChannelModel.update_at
+        })
+    }
+
+}
