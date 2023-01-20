a631b8cd8de0b5efe436084bdf3609d25ed38fd5
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-21 00:54:24 +0500

    [*] Rewrite realization console logger

--- src/components/logger/console.ts
+++ src/components/logger/console.ts
@@ -1,23 +1,41 @@
-import {LogData, Logger} from "../abstractions/logger";
+import {LoggerDto, Logger} from "../abstractions/logger";
 import {injectable} from "inversify";
+
+interface LoggerData extends Omit<LoggerDto, "date"> {
+    date: string
+}
+
+interface LoggerResultData extends Omit<LoggerData, "error"> {}
+
+interface LoggerErrorData extends Omit<LoggerData, "result"> {}
 
 @injectable()
 export class ConsoleLogger implements Logger {
-    public async print(logData: LogData): Promise<void> {
-        if (!logData.date) logData.date = new Date()
+    public async print(loggerDto: LoggerDto): Promise<void> {
+        let loggerData: LoggerData = {
+            result: loggerDto.result,
+            error: loggerDto.error,
+            date: this.printTime(loggerDto.date)
+        }
 
-        if(logData.error)
-            this.printError(logData)
+        if(loggerData.error)
+            this.printError(loggerData)
 
-        if(logData.result)
-            this.printResult(logData)
+        if(loggerData.result)
+            this.printResult(loggerData)
     }
 
-    private printError(logData: Omit<LogData, "result">) {
-        console.log(logData.date, "-", "ERROR", "-", logData.error)
+    private printResult(loggerData: LoggerResultData) {
+        console.info(loggerData.date, "-", "RESULT", "-", loggerData.result)
     }
 
-    private printResult(logData: Omit<LogData, "error">) {
-        console.log(logData.date, "-", "RESULT", "-", logData.result)
+    private printError(loggerData: LoggerErrorData) {
+        console.error(loggerData.date, "-", "ERROR", "-", loggerData.error)
+    }
+
+    private printTime(date: Date = new Date()): string {
+        let timeString = Intl.DateTimeFormat("mn-MN", {dateStyle: "short", timeStyle: "medium"}).format(date)
+
+        return `${timeString}.${date.getMilliseconds()}`
     }
 }
