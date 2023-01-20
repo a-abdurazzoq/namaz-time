612245d2a5f8d595b82070a80cfbc1ee697d93ff
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-21 02:18:02 +0500

    [*] return string

--- src/controllers/sending-prayer-times/index.ts
+++ src/controllers/sending-prayer-times/index.ts
@@ -4,7 +4,6 @@ import {injectable} from "inversify";
 @injectable()
 export class SendingPrayerTimesController implements Controller {
     async execute(): Promise<any> {
-        console.log("Controller executed")
-        return
+        return "Controller executed"
     }
 }
