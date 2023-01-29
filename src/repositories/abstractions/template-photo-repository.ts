472be1ac6c25442d87b0f729a287f0d121611c51
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-29 13:24:07 +0500

    [+] Add method generatePhotoByChatForSendingPrayerTimes for generation photo with prayer times

--- src/repositories/abstractions/template-photo-repository.ts
+++ src/repositories/abstractions/template-photo-repository.ts
@@ -1,5 +1,6 @@
-import {TemplatePhoto} from "../../domain/entities";
+import {ChatForSendingPrayerTimes, TemplatePhoto} from "../../domain/entities";
 
 export interface TemplatePhotoRepository {
     getById(id: string): Promise<TemplatePhoto>;
+    generatePhotoByChatForSendingPrayerTimes(chatForSendingPrayerTimes: ChatForSendingPrayerTimes): Promise<Buffer>;
 }
