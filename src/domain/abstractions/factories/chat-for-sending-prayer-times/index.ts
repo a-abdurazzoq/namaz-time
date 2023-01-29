1c756e72436d58b9f335450af128d84aebfec6c3
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-29 13:20:55 +0500

    [+] Add property template photo

--- src/domain/abstractions/factories/chat-for-sending-prayer-times/index.ts
+++ src/domain/abstractions/factories/chat-for-sending-prayer-times/index.ts
@@ -1,9 +1,10 @@
 import {Factory} from "../factory";
-import {ChatForSendingPrayerTimes, TelegramChannel} from "../../../entities";
+import {ChatForSendingPrayerTimes, TelegramChannel, TemplatePhoto} from "../../../entities";
 
 export interface ChatForSendingPrayerTimesDto {
     id: string;
     telegramChannel: TelegramChannel;
+    templatePhoto: TemplatePhoto;
     chatId: number;
     timePerDay: number;
     nextTime: Date;
