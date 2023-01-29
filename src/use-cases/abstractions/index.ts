ec002a3210dae5d93e92aa825c2447e7058e648b
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-29 13:37:33 +0500

    [+] Realization abstract use case [*] Import all module

--- src/use-cases/abstractions/index.ts
+++ src/use-cases/abstractions/index.ts
@@ -1,1 +1,7 @@
-export * from "./use-case"
+export * from "./regular-sending-prayer-times/get-all-execute-time-chats-for-sending-use-case"
+export * from "./regular-sending-prayer-times/mass-send-prayer-times-to-telegram-channels-use-case"
+export * from "./regular-sending-prayer-times/send-prayer-times-to-telegram-channel-use-case"
+
+export interface UseCase<T, R> {
+    execute(params: T): Promise<R>
+}
