540c8aba20a638751c9fd1cd2efb1ef5dd310ad5
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-29 13:36:50 +0500

    [+] Realization abstract send prayer times to telegram channel use case for send prayer times to telegram channel

---
+++ src/use-cases/abstractions/regular-sending-prayer-times/send-prayer-times-to-telegram-channel-use-case.ts
@@ -0,0 +1,25 @@
+import {UseCase} from "../index";
+import {ChatForSendingPrayerTimes} from "../../../domain/entities";
+
+export interface SendPrayerTimesToTelegramChannelUseCase extends UseCase<SendPrayerTimesToTelegramChannel.Params, SendPrayerTimesToTelegramChannel.Response> {}
+
+export namespace SendPrayerTimesToTelegramChannel {
+    export type Params = ChatForSendingPrayerTimes
+
+    export type Response = Error | Success
+
+    export interface Error extends Result {
+        success: false;
+        error: any;
+    }
+
+    export interface Success extends Result {
+        success: true;
+    }
+
+    export interface Result {
+        id: string;
+        name: string;
+        success: boolean;
+    }
+}
