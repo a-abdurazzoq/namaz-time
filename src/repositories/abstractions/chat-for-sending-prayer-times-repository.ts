e13b25114768823f487ae52833e85f6b1f20dde2
Abdurazzoq <abdurazzox9655@gmail.com>
2023-01-29 13:23:31 +0500

    [+] Add method getAllLessAndEqualByNextTime for get all chats which it's time to send

--- src/repositories/abstractions/chat-for-sending-prayer-times-repository.ts
+++ src/repositories/abstractions/chat-for-sending-prayer-times-repository.ts
@@ -2,4 +2,6 @@ import {ChatForSendingPrayerTimes} from "../../domain/entities";
 
 export interface ChatForSendingPrayerTimesRepository {
     getById(id: string): Promise<ChatForSendingPrayerTimes>;
+
+    getAllLessAndEqualByNextTime(date: Date): Promise<ChatForSendingPrayerTimes[]>;
 }
