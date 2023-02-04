9619e7a0ba861d1c3db69cdd4d27a7f452cb3047
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-05 01:01:25 +0500

    [+] Realization generatePhotoByChatForSendingPrayerTimes method

--- src/repositories/template-photo-repository.ts
+++ src/repositories/template-photo-repository.ts
@@ -1,24 +1,52 @@
-import {TemplatePhotoRepository, UserRepository} from "./abstractions";
 import {inject, injectable} from "inversify";
+
+import {
+    GeneratePhotoByChatForSendingPrayerTimesParams,
+    PrayersInDayRepository,
+    TemplatePhotoRepository,
+    UserRepository
+} from "./abstractions";
 import {TemplatePhotoFactory} from "../domain/abstractions/factories";
 import {Symbols} from "../dependencies/symbols";
 import {TemplatePhoto} from "../domain/entities";
 import {TemplatePhotoModel, ITemplatePhotoModel} from "../models";
+import {TemplatePhotoService} from "../services/abstractions/template-photo-service";
+import {IslamicCalendarRepository} from "./abstractions/islamic-calendar";
 
 @injectable()
 export class TemplatePhotoRepositoryImpl implements TemplatePhotoRepository {
     constructor(
         @inject(Symbols.Factories.TemplatePhoto) private templatePhotoFactory: TemplatePhotoFactory,
-        @inject(Symbols.Repositories.User) private userRepository: UserRepository
+        @inject(Symbols.Services.TemplatePhoto) private templatePhotoService: TemplatePhotoService,
+        @inject(Symbols.Repositories.User) private userRepository: UserRepository,
+        @inject(Symbols.Repositories.IslamicCalendar) private islamicCalendarRepository: IslamicCalendarRepository,
+        @inject(Symbols.Repositories.PrayersInDay) private prayersInDayRepository: PrayersInDayRepository
     ) {}
 
     public async getById(id: string): Promise<TemplatePhoto> {
-        let getTemplatePhoto = await TemplatePhotoModel.findById<ITemplatePhotoModel>(id)
+        const getTemplatePhoto = await TemplatePhotoModel.findById<ITemplatePhotoModel>(id)
 
         if(!getTemplatePhoto)
             throw new Error("Template photo not found by id")
 
         return this.toEntity(getTemplatePhoto)
+    }
+
+    public async generatePhotoByChatForSendingPrayerTimes(params: GeneratePhotoByChatForSendingPrayerTimesParams): Promise<Buffer> {
+        const islamicCalendar = await this.islamicCalendarRepository.getDayByGregorianTime(params.necessaryDate)
+        const prayerInDay = await this.prayersInDayRepository.getIslamicCalendarId(islamicCalendar.getId())
+
+        return this.templatePhotoService.photoGeneration({
+            htmlFileName: params.chatForSendingPrayerTimes.getTemplatePhoto().getFileName(),
+            fajr: prayerInDay.getPrayerTimes().getFajr(),
+            shurooq: prayerInDay.getPrayerTimes().getShurooq(),
+            dhuhr: prayerInDay.getPrayerTimes().getIsha(),
+            asr: prayerInDay.getPrayerTimes().getIsha(),
+            maghrib: prayerInDay.getPrayerTimes().getIsha(),
+            isha: prayerInDay.getPrayerTimes().getIsha(),
+            gregorianFullDate: prayerInDay.getPrayerTimes().getIsha(),
+            islamicFullDate: prayerInDay.getPrayerTimes().getIsha(),
+        })
     }
 
     private async toEntity(templatePhotoModel: ITemplatePhotoModel): Promise<TemplatePhoto> {
