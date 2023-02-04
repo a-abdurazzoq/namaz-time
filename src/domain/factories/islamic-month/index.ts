13512fe94b3c95e99e1c099653d856348dc1a45a
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-05 00:28:03 +0500

    [+] Injected class

--- src/domain/factories/islamic-month/index.ts
+++ src/domain/factories/islamic-month/index.ts
@@ -1,6 +1,8 @@
 import {IslamicMonthDto, IslamicMonthFactory} from "../../abstractions/factories/islamic-month";
 import {IslamicMonth} from "../../entities";
+import {injectable} from "inversify";
 
+@injectable()
 export class IslamicMonthFactoryImpl implements IslamicMonthFactory {
     create(dto: IslamicMonthDto): IslamicMonth {
         return new IslamicMonth(
