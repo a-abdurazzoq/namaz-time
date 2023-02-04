2ca81f483a39a172167baf91585ef7b45892f1cb
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-05 01:02:08 +0500

    [*] Change date property to gregorianFullDate; [*] Change sun property to shurooq; [+] islamicFullDate

--- src/services/abstractions/template-photo-service/index.ts
+++ src/services/abstractions/template-photo-service/index.ts
@@ -4,9 +4,10 @@ export interface TemplatePhotoService {
 
 export interface PhotoGenerationParams {
     htmlFileName: string;
-    date: string;
+    gregorianFullDate: string;
+    islamicFullDate: string;
     fajr: string;
-    sun: string;
+    shurooq: string;
     dhuhr: string;
     asr: string;
     maghrib: string;
