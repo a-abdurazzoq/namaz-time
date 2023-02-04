320e0f7c3ec2295e59dae3a00eec54f519b6b9b1
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-05 00:42:41 +0500

    [+] Realization prayer times model

---
+++ src/models/prayer-times.ts
@@ -0,0 +1,74 @@
+import {Schema, model, Types, Document} from "mongoose"
+
+export interface IPrayerTimesModel extends Document {
+    islamic_calendar_id: Types.ObjectId;
+    prayer_times: {
+        fajr: string;
+        shurooq: string;
+        dhuhr: string;
+        asr: string;
+        maghrib: string;
+        isha: string;
+    };
+    create_at: Date;
+    update_at: Date;
+}
+
+const validatePrayerTime = {
+    validate: {
+        validator(date: string) {
+            return /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(date);
+        },
+        message(props: any) {
+            return `${props.value} is not a valid phone number!`
+        }
+    },
+}
+
+const PrayerTimesSchema = new Schema({
+    islamic_calendar_id: {
+        type: Types.ObjectId,
+        required: true
+    },
+    prayer_times: {
+        fajr: {
+            type: String,
+            required: true,
+        },
+        shurooq: {
+            type: String,
+            required: true,
+            validate: validatePrayerTime
+        },
+        dhuhr: {
+            type: String,
+            required: true,
+            validate: validatePrayerTime
+        },
+        asr: {
+            type: String,
+            required: true,
+            validate: validatePrayerTime
+        },
+        maghrib: {
+            type: String,
+            required: true,
+            validate: validatePrayerTime
+        },
+        isha: {
+            type: String,
+            required: true,
+            validate: validatePrayerTime
+        },
+    },
+    create_at: {
+        type: Date,
+        default: Date.now()
+    },
+    update_at: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+export const IPrayerTimesModel = model<IPrayerTimesModel>("prayer_times", PrayerTimesSchema)
