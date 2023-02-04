78d68bb305b4085c830346a7cced6bbb6507ebb0
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-05 00:47:08 +0500

    [+] Realization abstract template photo repository

--- src/repositories/abstractions/template-photo-repository.ts
+++ src/repositories/abstractions/template-photo-repository.ts
@@ -1,6 +1,11 @@
 import {ChatForSendingPrayerTimes, TemplatePhoto} from "../../domain/entities";
 
+export interface GeneratePhotoByChatForSendingPrayerTimesParams {
+    chatForSendingPrayerTimes: ChatForSendingPrayerTimes;
+    necessaryDate: Date;
+}
+
 export interface TemplatePhotoRepository {
     getById(id: string): Promise<TemplatePhoto>;
-    generatePhotoByChatForSendingPrayerTimes(chatForSendingPrayerTimes: ChatForSendingPrayerTimes): Promise<Buffer>;
+    generatePhotoByChatForSendingPrayerTimes(params: GeneratePhotoByChatForSendingPrayerTimesParams): Promise<Buffer>;
 }
