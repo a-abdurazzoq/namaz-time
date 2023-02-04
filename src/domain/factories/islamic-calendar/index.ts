8f5ce2025c189200fc1abec9150b339400ff387e
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-05 00:23:41 +0500

    [+] Implementation Islamic calendar factory

---
+++ src/domain/factories/islamic-calendar/index.ts
@@ -0,0 +1,16 @@
+import {IslamicCalendar} from "../../entities";
+import {IslamicCalendarDto, IslamicCalendarFactory} from "../../abstractions/factories/islamic-calendar";
+
+export class IslamicCalendarFactoryImpl implements IslamicCalendarFactory {
+    create(dto: IslamicCalendarDto): IslamicCalendar {
+        return new IslamicCalendar(
+            dto.id,
+            dto.year,
+            dto.dayNumber,
+            dto.month,
+            dto.gregorianTime,
+            dto.createAt,
+            dto.updateAt
+        )
+    }
+}
