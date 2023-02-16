dc93de15d546170ddbd9c972d475e7e4611f5b72
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-16 19:08:18 +0500

    [+] export all module

--- src/use-cases/abstractions/index.ts
+++ src/use-cases/abstractions/index.ts
@@ -1,6 +1,12 @@
-export * from "./regular-sending-prayer-times/get-all-execute-time-chats-for-sending-use-case"
-export * from "./regular-sending-prayer-times/mass-send-prayer-times-to-telegram-channels-use-case"
-export * from "./regular-sending-prayer-times/send-prayer-times-to-telegram-channel-use-case"
+export * from "./scheduler/regular-sending-prayer-times/mass-send-prayer-times-to-telegram-channels-use-case"
+export * from "./scheduler/regular-sending-prayer-times/get-all-execute-time-chats-for-sending-use-case"
+export * from "./scheduler/regular-sending-prayer-times/send-prayer-times-to-telegram-channel-use-case"
+
+export * from "./scheduler/update-prayers-per-month/update-prayers-per-month-use-case"
+
+export * from "./scheduler/update-islamic-calenadar/update-islamic-calendar-use-case"
+
+export * from "./api/namaz-time-api/prayer-times/get-today-prayer-times-use-case"
 
 export interface UseCase<T, R> {
     execute(params: T): Promise<R>
