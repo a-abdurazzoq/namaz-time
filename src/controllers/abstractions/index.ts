fcf788766e1052b4ba704577ee2987624812d69f
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-21 19:10:32 +0500

    [+] export all module

--- src/controllers/abstractions/index.ts
+++ src/controllers/abstractions/index.ts
@@ -1,5 +1,8 @@
 export * from "./scheduler-controller"
+
 export * from "./namaz-time/prayer-times/get-today-prayer-times-controller"
+
+export * from "./namaz-time/requests/create-request-for-register-controller"
 
 export interface Controller<T, R> {
     execute(params: T): Promise<R>
