384190d0b8aea9e449337581ab435c05bfb54ce0
Abdurazzoq <abdurazzox9655@gmail.com>
2023-02-21 11:13:58 +0500

    [*] Abstract Controller moved to index file

--- src/controllers/abstractions/controller.ts
+++
@@ -1,3 +0,0 @@
-export interface Controller<R = any> {
-    execute(): Promise<R>
-}
--- src/controllers/abstractions/index.ts
+++ src/controllers/abstractions/index.ts
@@ -1,1 +1,6 @@
-export * from "./controller"
+export * from "./scheduler-controller"
+export * from "./namaz-time/prayer-times/get-today-prayer-times-controller"
+
+export interface Controller<T, R> {
+    execute(params: T): Promise<R>
+}
