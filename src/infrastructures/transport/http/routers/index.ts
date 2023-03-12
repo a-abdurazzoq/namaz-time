b4d23cdb014bca446b089d6cdce341a3bdfb39a7
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-12 08:35:39 +0500

    [+] Implementation own type Request and Response

--- src/infrastructures/transport/http/routers/index.ts
+++ src/infrastructures/transport/http/routers/index.ts
@@ -1,5 +1,10 @@
+import {Request, Response} from "express";
+
 export * from "./post-for-telegram-router"
 export * from "./prayer-times-router"
 export * from "./request-router"
 
 export interface RouterBase {}
+
+export type RRequest<body> = Request<body>
+export type RResponse = Omit<Response, "json" | "jsonp" | "sendStatus" | "send" | "sendFile" | "sendDate" | "download">
