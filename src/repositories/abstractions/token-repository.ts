ed7accb4dd9f5014a6648a58e35cf5cd402cf128
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-13 01:03:19 +0500

    [+] Realization abstract Token repository

---
+++ src/repositories/abstractions/token-repository.ts
@@ -0,0 +1,16 @@
+import {Token, User} from "../../domain/entities";
+
+export interface TokenRepository {
+    create(user: User): Promise<Token>;
+    getByToken(token: string): Promise<Token | null>;
+    updateByUserId(user: User): Promise<Token>;
+    hasTokenByUserId(user: User): Promise<boolean>;
+    hasToken(token: string): Promise<boolean>;
+
+}
+
+export interface DataInToken {
+    id: string;
+    username: string;
+    phoneNumber: string;
+}
