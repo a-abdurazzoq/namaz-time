febf89adf205776b739650971515b3fb8de288ca
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-13 15:23:33 +0500

    [-] Remove abstract [+] Export all modules

--- src/infrastructures/transport/http/routers/index.ts
+++ src/infrastructures/transport/http/routers/index.ts
@@ -1,10 +1,4 @@
-import {Request, Response} from "express";
-
 export * from "./post-for-telegram-router"
+export * from "./authorization-router"
 export * from "./prayer-times-router"
 export * from "./request-router"
-
-export interface RouterBase {}
-
-export type RRequest<body> = Request<body>
-export type RResponse = Omit<Response, "json" | "jsonp" | "sendStatus" | "send" | "sendFile" | "sendDate" | "download">
