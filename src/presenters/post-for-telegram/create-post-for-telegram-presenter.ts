1378e27b23c1559bcc288d6f470870f7ad2bfc68
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-13 15:21:19 +0500

    [*] Rename namespace from CreatePostForTelegram to CreatePostForTelegramPresenterData

--- src/presenters/post-for-telegram/create-post-for-telegram-presenter.ts
+++ src/presenters/post-for-telegram/create-post-for-telegram-presenter.ts
@@ -1,12 +1,12 @@
 import {
-    CreatePostForTelegram,
+    CreatePostForTelegramPresenterData,
     CreatePostForTelegramPresenter
 } from "../abstractions/post-for-telegram/create-post-for-telegram-presenter";
 import {injectable} from "inversify";
 
 @injectable()
 export class CreatePostForTelegramPresenterImpl implements CreatePostForTelegramPresenter {
-    print(params: CreatePostForTelegram.Params): CreatePostForTelegram.Response {
+    print(params: CreatePostForTelegramPresenterData.Params): CreatePostForTelegramPresenterData.Response {
         return {
             message: "Ваш(а) пост запланирован"
         }
