e330ed5ef9df404ff6fb63a3c702477318f37329
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-13 15:18:56 +0500

    [+] Export all modules

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,7 @@
 export * from "./scheduler/sending-prayer-times"
 export * from "./scheduler/update-prayers-per-month"
+
+export * from "./namaz-time/prayer-times"
+export * from "./namaz-time/authorization"
+export * from "./namaz-time/post-for-telegram"
+export * from "./namaz-time/request"
