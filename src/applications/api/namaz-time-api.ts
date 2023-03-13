824cb5ee722ebd9ead9f0250c5d60c03d922a8ad
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-13 14:34:50 +0500

    [-] Remove trash code

--- src/applications/api/namaz-time-api.ts
+++ src/applications/api/namaz-time-api.ts
@@ -6,14 +6,13 @@ import path from "path"
 import {ApplicationRunnerImpl} from "../../infrastructures/application";
 import {ContainerImpl} from "../../dependencies/container";
 import {namazTimeModule} from "../../dependencies/modules";
-import {Symbols} from "../../dependencies/symbols";
 
 dotenv.config({path: path.join(__dirname, "../../../.env")})
 
 const container = new ContainerImpl([
     namazTimeModule
 ])
-container.get(Symbols.Infrastructures.Http.Decorator)
+
 const application = new ApplicationRunnerImpl(container)
 
 
