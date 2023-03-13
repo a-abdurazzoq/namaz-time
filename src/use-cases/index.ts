27014a66346a143ae0b5840f1246100e9a2e59c1
Abdurazzoq <abdurazzox9655@gmail.com>
2023-03-13 15:09:47 +0500

    [+] Export all modules

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -7,3 +7,7 @@ export * from "./scheduler/update-prayers-per-month/update-prayers-per-month-use
 export * from "./scheduler/update-islamic-calenadar/update-islamic-calendar-use-case"
 
 export * from "./api/namaz-time-api/prayer-times/get-prayer-times-use-case"
+export * from "./api/namaz-time-api/request/create-request-for-register-use-case"
+export * from "./api/namaz-time-api/post-for-telegram/create-post-for-telegram-use-case"
+export * from "./api/namaz-time-api/authorization/login-authorization-use-case"
+export * from "./api/namaz-time-api/authorization/registration-authorization-use-case"
